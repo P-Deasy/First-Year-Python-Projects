d9a5f1027b0d9ebe8e7707d0c32c98e7aa2e8a8c
Paul Deasy <61508859+P-Deasy@users.noreply.github.com>
2020-02-28 14:16:27 +0000

    Update functions_5.py

--- functions_5.py
+++ functions_5.py
@@ -1,16 +1,11 @@
-# ScriptName: functions.py
-# Author: 118312303 Paul Deasy
-
-# template for calling functions in another file
-
 import random
 
 
-def print_function():
-    print("I'm in another file :)")
-
-
 def is_stairs(s):
+    '''
+    Check if lists of either numbers or letters are + or - 1
+    in postion 
+    '''
     n = 1
     y = True
     x = True
@@ -37,6 +32,9 @@ def is_stairs(s):
 
 
 def factorial(n=0):
+    '''
+    Calculates factorial of a number
+    '''
     i = 1
     if n == 0:
         return 1
@@ -45,7 +43,19 @@ def factorial(n=0):
     return i
 
 
+#If you've seen Gremlins the movie, then you
+#know the little furry creatures known as Mogwai must be cared for with
+#consideration for these three rules:
+#i. Don't get them wet
+#ii. Don't expose them to bright light.
+#iii. Don't feed them after midnight.
+#The folowing functions determine the fate of the gremlin
+
+
 def is_okay_to_feed(time):
+    '''
+    Determines if feeding time is okay
+    '''
     if time in range(0, 4):
         return False
     if time in range(5, 24):
@@ -53,29 +63,38 @@ def is_okay_to_feed(time):
 
 
 def is_wet():
-    return True or False
+    '''
+    Randomly determines if Mogwai gets wet or not
+    '''
+    return random.choice([True, False])
 
 
 def is_to_bright(lux_level):
+    '''
+    Determines if light is too bright or not
+    '''
     if lux_level > 80:
         return True
     else:
         return False
 
 
-def gremlins(name='Gizmo'):
+def gremlins(name ='Gizmo'):
+    '''
+    Determines what happens to your named Mogwai
+    '''
     title = 'the one and only'
     for i in range(5):
         # using true or false to add randomness
-        x = True or False
+        x = random.choice([True, False])
         if x is True:
             if not is_okay_to_feed(random.randint(0, 24)):
                 name = 'Stripe'
-        z = True or False
+        z = random.choice([True, False])
         if z is True:
             if is_to_bright(random.randint(0, 100)):
                 return name + ' ' + title + ' is very dead'
-        c = True or False
+        c = random.choice([True, False])
         if c is True:
             if is_wet() is True:
                 title = 'the third'
