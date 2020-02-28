eac401693807e961df42e6d2105217861d2da433
Paul Deasy <61508859+P-Deasy@users.noreply.github.com>
2020-02-28 13:47:35 +0000

    Update functions_2.py

--- functions_2.py
+++ functions_2.py
@@ -1,21 +1,14 @@
-# ScriptName: functions.py
-# Author: Paul Deasy 118312303
-
-# template for calling functions in another file
-
-
-def print_function():
-    print("I'm in another file :)")
-
-
 def seasons():
+    '''
+    Select the season based on the input value where 1=Winter,
+    2=Spring, 3=Summer, and 4=Autumn.
+    '''
     while True:
         try:
             season = int(input("Give me a number:"))
             break
         except:
             print("Enter a number")
-    # Added list
     seasons = ['Winter', 'Spring', 'Summer', 'Autumn']
     if season == 1:
         print(seasons[0])
@@ -30,7 +23,9 @@ def seasons():
 
 
 def grades():
-    # Added try except statement
+    '''
+    Changes numerical grades to Letter grades and vice-versa
+    '''
     try:
         grade = int(input("Enter a result:"))
         if 100 >= grade >= 85:
@@ -67,6 +62,11 @@ def grades():
 
 
 def fizz_buzz():
+    '''
+    For multiples of
+    divisor_1 the function return “Fizz”. For multiples of divisor_2, return “Buzz”. For
+    numbers which are multiples of both divisor_1 and divisor_2 return “FizzBuzz” 
+    '''
     while True:
         try:
             fbnum = float(input("Enter a number:"))
@@ -86,6 +86,9 @@ def fizz_buzz():
 
 
 def slice_reverse(b):
+    '''
+    determine if the input_value is a palindrome
+    '''
     a = list(b)
     if a[:] == a[::-1]:
         print(True)
@@ -94,6 +97,10 @@ def slice_reverse(b):
 
 
 def add_to_list(value, mylist):
+    '''
+    The function will add value to the list if
+    the list does not already contain the value
+    '''
     if value not in mylist:
        mylist.append(value)
     print(mylist)
